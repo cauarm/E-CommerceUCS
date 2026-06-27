746cb63d6f6e10e1636d5234c10a7b8fbd85c7ac
Cauã <caua.riquelme1507@gmail.com>
2026-06-27 16:35:54 -0300

    - final version - 2.0

--- src/Ecommerce/app/Main.java
+++ src/Ecommerce/app/Main.java
@@ -2,6 +2,8 @@ package Ecommerce.app;
 
 import Ecommerce.view.Menu;
 
+// Foi utilizado IA para testes e revisão do código, seus métodos e auxilios em questão de erros.
+
 public class Main{
 
     public static void main(String[] args) {
